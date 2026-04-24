6d215e54b31b71bccf2cd30a5e187a300625a34c
cseitapp <cseitapp@gmail.com>
2026-04-24 10:09:01 +0700

    update get profile

--- server/api/profile.get.ts
+++ server/api/profile.get.ts
@@ -1,6 +1,27 @@
 export default defineEventHandler((event) => {
+  console.log(123);
 
   const cookies = parseCookies(event);
+  const origin = getHeader(event, "origin");
+  console.log(origin);
+
+  // allow only your frontend domain
+  const allowedOrigins = ["https://edocument.cselao.la"];
+
+  if (origin && allowedOrigins.includes(origin)) {
+    setHeader(event, "Access-Control-Allow-Origin", origin);
+    setHeader(event, "Access-Control-Allow-Credentials", "true");
+    setHeader(
+      event,
+      "Access-Control-Allow-Methods",
+      "GET,POST,PUT,DELETE,OPTIONS"
+    );
+    setHeader(
+      event,
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+  }
 
   const token = cookies.token;
 
